454c93c6244119e45963a6123aeb4824dec9fc0a
Lars Zwerver <larszwerver@gmail.com>
2024-09-28 17:12:52 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,60 @@
+// counter
+var count = 0;
+var counter = document.getElementById("counter")
+counter.innerText = count;
+
+
+var redButton = document.getElementById("button--red");
+redButton.onclick = function(){
+    if(count > -10)
+    count -= 1;
+else{
+    count = 0;
+}
+    counter.innerText = count;
+}
+
+var greenButton = document.getElementById("button--green");
+greenButton.onclick = function(){
+    if(count < 10)
+    count += 1;
+else{
+    count = 0;
+}
+    counter.innerText = count;
+}
+
+// Stopwatch
+var minutes = 0;
+var seconds = 0;
+var everySecond = false 
+var watch = document.getElementById("stopwatch");
+watch.innerText = minutes + ":0" + seconds;
+
+var start = document.getElementById("button--start");
+start.onclick = function(){
+// elke seconden iets uitvoeren
+
+if(everySecond !== false) {
+    clearInterval(everySecond)
+}
+
+everySecond = setInterval(function()  {
+    seconds += 1;
+    if(seconds > 60){
+        seconds = 0;
+        minutes += 1;
+    }
+    if (seconds > 9){
+        watch.innerText = minutes + ":" + seconds;
+    }
+    else {
+        watch.innerText = minutes + ":0" + seconds;
+    }
+}, 1000) 
+}
+
+var stop = document.getElementById("button--stop");
+stop.onclick = function(){
+    clearInterval(everySecond);
+}
